b9f352df250fae7a2c51f87610612806649366c0
Shantanu <sparab@umd.edu>
2022-12-09 12:25:29 -0500

    [2.9.4] add_robot method added

--- include/project_chakravyu/master.hpp
+++ include/project_chakravyu/master.hpp
@@ -1,6 +1,6 @@
 /**
  * @file master.hpp
- * @author your name (you@domain.com)
+ * @author Shantanu Parab (sparab@umd.edu)
  * @brief 
  * @version 0.1
  * @date 2022-12-09
@@ -17,17 +17,18 @@ using TWIST = geometry_msgs::msg::Twist;
 class Master: public rclcpp::Node {
  private:
     std::vector<Robot> robot_array;
+    std::vector<rclcpp::Publisher<TWIST>::SharedPtr> publisher_array;
      ////////////////////////////////////////
   // member variables
   ////////////////////////////////////////
     // rclcpp::Subscription<IMAGE>::SharedPtr subscription_;
-    rclcpp::Publisher<TWIST>::SharedPtr    publisher_;
+    // rclcpp::Publisher<TWIST>::SharedPtr    publisher_;
     rclcpp::TimerBase::SharedPtr           timer_;
 
  public:
     Master();
     void process_callback();
-    // add robot
+    void add_robot(int robot_id);
     // publish
     // get_pose
     // set_location
--- src/master.cpp
+++ src/master.cpp
@@ -22,8 +22,9 @@ using TWIST = geometry_msgs::msg::Twist;
 
 Master::Master():Node("master_node") {
 // creates publisher to publish /cmd_vel topic
-auto pubTopicName = "cmd_vel";
-this->publisher_ = this->create_publisher<TWIST> (pubTopicName, 10);
+// auto pubTopicName = "cmd_vel";
+// this->publisher_ = this->create_publisher<TWIST> (pubTopicName, 10);
+this->add_robot(1);
 
 // create a 10Hz timer for processing
 auto processCallback = std::bind(&Master::process_callback, this);
@@ -32,8 +33,14 @@ this->timer_ = this->create_wall_timer(100ms, processCallback);
 
 void Master::process_callback() {
 auto message = TWIST();
-this->publisher_->publish(message);
+this->publisher_array[0]->publish(message);
 RCLCPP_INFO_STREAM(this->get_logger(), "State = STOP");
+}
+
+void Master::add_robot(int robot_id) {
+    // this->robot_array.push_back(Robot(robot_id));
+    auto pubTopicName = "robot_"+std::to_string(robot_id)+ "cmd_vel";
+    this->publisher_array.push_back(this->create_publisher<TWIST> (pubTopicName, 10));
 }
 
 
