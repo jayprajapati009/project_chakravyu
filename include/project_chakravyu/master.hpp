fff3af13d8e4d0aeec0fad8283adbd3b62ffc34b
Shantanu <sparab@umd.edu>
2022-12-11 23:51:13 -0500

    [2.9.6] Creating Publisher in Robot Class

--- include/project_chakravyu/master.hpp
+++ include/project_chakravyu/master.hpp
@@ -11,30 +11,34 @@
 #ifndef INCLUDE_PROJECT_CHAKRAVYU_MASTER_HPP_
 #define INCLUDE_PROJECT_CHAKRAVYU_MASTER_HPP_
 #include <vector>
+#include <memory>
 #include "project_chakravyu/robot.hpp"
 #include <nav_msgs/msg/odometry.hpp>
 
 using TWIST = geometry_msgs::msg::Twist;
 using ODOM = nav_msgs::msg::Odometry;
+using ROBOT_PTR = std::shared_ptr<Robot>;
+std::shared_ptr<CallbackFunction>
 
 class Master: public rclcpp::Node {
  private:
-    std::vector<Robot> robot_array;
-    std::vector<rclcpp::Publisher<TWIST>::SharedPtr> publisher_array;
-    std::vector<rclcpp::Subscription<ODOM>::SharedPtr> pose_subscriber_array;
+    int numNodes = 5;
+    std::vector<ROBOT_PTR> robot_array_  = std::vector<ROBOT_PTR>(numNodes);
+    //  std::vector<rclcpp::Publisher<TWIST>::SharedPtr> publisher_array;
+    //  std::vector<rclcpp::Subscription<ODOM>::SharedPtr> pose_subscriber_array;
      ////////////////////////////////////////
   // member variables
   ////////////////////////////////////////
     // rclcpp::Subscription<IMAGE>::SharedPtr subscription_;
     // rclcpp::Publisher<TWIST>::SharedPtr    publisher_;
-    rclcpp::TimerBase::SharedPtr           timer_;
-    ODOM odometry;
+    rclcpp::TimerBase::SharedPtr  timer_;
+    
 
  public:
     Master();
     void process_callback();
     void add_robot(int robot_id);
-    void subscribe_callback(const ODOM& msg);
+    // void subscribe_callback(const ODOM& msg);
     // publish
     // get_pose
     // set_location
