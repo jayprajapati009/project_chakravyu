c6d577f6b21545dedb1c3698e8ea1719b5e2ec4f
Shantanu <sparab@umd.edu>
2022-12-15 15:37:48 -0500

    [2.11.5] Adding More Test Cases

--- test/test.cpp
+++ test/test.cpp
@@ -24,7 +24,7 @@
 
 /**
  * @file test.cpp
- * @author Jay Prajapati (jayp@umd.edu)
+ * @author Jay Prajapati (jayp@umd.edu) Shantanu Parab (sparab@umd.edu)
  * @brief test cases for the module
  * @version 0.1
  * @date 2022-12-15
@@ -39,22 +39,26 @@
 #include <rclcpp/rclcpp.hpp>
 
 #include "std_msgs/msg/string.hpp"
+#include "project_chakravyu/robot.hpp"
+#include "project_chakravyu/master.hpp"
 
 /**
  * @brief Testing Publisher count
  *
  */
-class TestingPublisher : public testing::Test
+class TestSuite : public testing::Test
 {
 protected:
   rclcpp::Node::SharedPtr test_node_;
+  std::shared_ptr<Robot> robot;
+  std::shared_ptr<Master> master;
 };
 
 ///////////////////////////////////////////////////////////
 /// Tests
 ///////////////////////////////////////////////////////////
 
-TEST_F(TestingPublisher, test_publisher_count)
+TEST_F(TestSuite, test_publisher_count)
 {
   test_node_ = rclcpp::Node::make_shared("test_publisher");
   auto test_pub_1 =
@@ -64,6 +68,112 @@ TEST_F(TestingPublisher, test_publisher_count)
 
   auto number_of_publishers = test_node_->count_publishers("topic");
   EXPECT_EQ(2, static_cast<int>(number_of_publishers));
+}
+TEST_F(TestSuite, robot_testing)
+{
+  auto r_namespace = "robot_" + std::to_string(1);
+  auto nodename = "robot_" + std::to_string(1) + "_controller";
+  robot = std::make_shared<Robot>(nodename, r_namespace);
+
+  auto number_of_publishers = robot->count_publishers("/" + r_namespace + "/cmd_vel");
+  EXPECT_EQ(1, static_cast<int>(number_of_publishers));
+}
+//Testing Number of Slaves Spawned Counting the publihsers
+TEST_F(TestSuite, slave_spawn_testing_publishers)
+{ int nodes = 10;
+  int pub_count = 0;
+  rclcpp::executors::MultiThreadedExecutor exec;
+  std::vector<std::shared_ptr<Robot>> robot_array;
+  for (int i = 0; i < nodes; i++) {
+    auto r_namespace = "robot_" + std::to_string(i);
+    auto nodename = "robot_" + std::to_string(i) + "_controller";
+    auto robot = std::make_shared<Robot>(nodename, r_namespace);
+    exec.add_node(robot);
+    robot_array.push_back(robot);
+  }
+  auto node = std::make_shared<Master>(robot_array, static_cast<int>(nodes));
+  for (int i = 0; i < nodes; i++) {
+    auto r_namespace = "robot_" + std::to_string(i);
+    auto number_of_publishers = robot_array[i]->count_publishers("/" + r_namespace + "/cmd_vel");
+    pub_count = pub_count+static_cast<int>(number_of_publishers);
+  }
+  EXPECT_EQ(nodes, pub_count);
+}
+
+//Testing Number of Slaves Spawned_Counting the subscribers
+TEST_F(TestSuite, slave_spawn_testing_subscribers)
+{ int nodes = 10;
+  int pub_count = 0;
+  rclcpp::executors::MultiThreadedExecutor exec;
+  std::vector<std::shared_ptr<Robot>> robot_array;
+  for (int i = 0; i < nodes; i++) {
+    auto r_namespace = "robot_" + std::to_string(i);
+    auto nodename = "robot_" + std::to_string(i) + "_controller";
+    auto robot = std::make_shared<Robot>(nodename, r_namespace);
+    exec.add_node(robot);
+    robot_array.push_back(robot);
+  }
+  auto node = std::make_shared<Master>(robot_array, static_cast<int>(nodes));
+  for (int i = 0; i < nodes; i++) {
+    auto r_namespace = "robot_" + std::to_string(i);
+    auto number_of_subs = robot_array[i]->count_subscribers("/" + r_namespace + "/odom");
+    pub_count = pub_count+static_cast<int>(number_of_subs);
+  }
+  EXPECT_EQ(nodes, pub_count);
+}
+
+// Check Method Compute Distance
+TEST_F(TestSuite, compute_distance)
+{ 
+  int n = 1;
+  auto r_namespace = "robot_" + std::to_string(n);
+  auto nodename = "robot_" + std::to_string(n) + "_controller";
+  robot = std::make_shared<Robot>(nodename, r_namespace);
+  std::pair<double, double> goal{10.0, 10.0};
+  std::pair<double, double> loc{0.0, 0.0};
+  double distance = robot->compute_distance(loc, goal);
+  double ex = 14.1421;
+  EXPECT_NEAR(ex, distance,0.1);
+}
+
+// Check Method Nomrmalize Angle 
+TEST_F(TestSuite, normalize_angle)
+{ 
+  int n = 1;
+  auto r_namespace = "robot_" + std::to_string(n);
+  auto nodename = "robot_" + std::to_string(n) + "_controller";
+  robot = std::make_shared<Robot>(nodename, r_namespace);
+  double angle = robot->normalize_angle(-10.14);
+  double ex = 2.42637;
+  EXPECT_NEAR(ex, angle,0.1);
+}
+
+// Check Method Nomrmalize Angle Positive
+TEST_F(TestSuite, normalize_angle_positive)
+{ 
+  int n = 1;
+  auto r_namespace = "robot_" + std::to_string(n);
+  auto nodename = "robot_" + std::to_string(n) + "_controller";
+  robot = std::make_shared<Robot>(nodename, r_namespace);
+  double angle = robot->normalize_angle_positive(-7.29);
+  double ex = 5.2763;
+  EXPECT_NEAR(ex, angle,0.1);
+}
+
+// Check Method Yaw From Quaternions
+TEST_F(TestSuite, compute_yaw_from_quaternion)
+{ 
+  int n = 1;
+  auto r_namespace = "robot_" + std::to_string(n);
+  auto nodename = "robot_" + std::to_string(n) + "_controller";
+  robot = std::make_shared<Robot>(nodename, r_namespace);
+  robot->m_orientation.x=3.0;
+  robot->m_orientation.y=5.0;
+  robot->m_orientation.z=4.0;
+  robot->m_orientation.w=6.0;
+  double yaw = robot->compute_yaw_from_quaternion();
+  double ex = 1.51955;
+  EXPECT_NEAR(ex, yaw, 0.1);
 }
 
 int main(int argc, char **argv)
