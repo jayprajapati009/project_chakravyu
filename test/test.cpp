f49ebfc9264c58900d6196dd4d876c19d8a46e0d
Jay Prajapati <jayprajapati9900@gmail.com>
2022-12-15 06:53:07 -0500

    [2.11.2] Testing publisher

--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 #include <gtest/gtest.h>
+#include <stdlib.h>
 
 #include <rclcpp/rclcpp.hpp>
+
+#include "std_msgs/msg/string.hpp"
+
+class TestingPublisher : public testing::Test
+{
+protected:
+    rclcpp::Node::SharedPtr test_node_;
+};
+
+TEST_F(TestingPublisher, test_publisher_count)
+{
+    test_node_ = rclcpp::Node::make_shared("test_publisher");
+    auto test_pub_1 =
+        test_node_->create_publisher<std_msgs::msg::String>("topic", 10.0);
+    auto test_pub_2 =
+        test_node_->create_publisher<std_msgs::msg::String>("topic", 10.0);
+
+    auto number_of_publishers = test_node_->count_publishers("topic");
+    EXPECT_EQ(2, static_cast<int>(number_of_publishers));
+}
 
 int main(int argc, char **argv)
 {
